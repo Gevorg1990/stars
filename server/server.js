8fe608836a2f7664ea3429d2eacf83abf1b75178
gevorg_gasparyan@helix.am <gevorg_gasparyan@helix.am>
2024-08-23 10:52:49 +0400

    test

--- server/server.js
+++ server/server.js
@@ -88,5 +88,5 @@ app.post('/global-rating', (req, res) => {
 
 const PORT = 3000;
 app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:3000/:${PORT}`);
+    console.log(`Server is running on http://localhost:${PORT}`);
 });
